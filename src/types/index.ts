374936b9cf46fe4f79538c5e8ee41447a4d4600e
Steffen <Steffenknaup@hotmail.de>
2024-11-17 00:21:35 +0100

    :sparkles: Add some random generated colors

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,6 +1,7 @@
 export * from './wled/index';
 
 export * from './ddp-packet';
+export * from './led-colors';
 export * from './led-strip';
 export * from './websocket-add-response';
 export * from './websocket-callback';
